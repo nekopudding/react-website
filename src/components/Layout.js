291eac3aa0c2fe43f94848279ab735c6a8c863cc
nekopudding <deany.y.ca@gmail.com>
2021-09-19 17:01:01 -0700

    Updates

--- src/components/Layout.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-
-export const Layout = (props) => (
-  <Container> {/*container is part of bootstrap, centers and prevents overflowing text*/}
-    {props.children}
-  </Container>
-)
